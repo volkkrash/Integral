8291ebe21cc035569b1330ead6c4969d7fc6efae
vlad <vlad-1996@mail.ru>
2018-04-04 23:12:14 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.slider-who-needs').slick({
+    dots: true,
+    infinite: true,
+ 		speed: 300,
+ 		slidesToShow: 1,
+ 		adaptiveHeight: true,
+ 		autoplay: true,
+  	autoplaySpeed: 5000
+  });
+});
+
